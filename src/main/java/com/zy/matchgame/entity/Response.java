269001d689f6664de445c2f7d3f410416d7b296b
TingYan <xym13662613887@outlook.com>
2024-08-05 13:33:46 +0800

    响应实体父类

---
+++ src/main/java/com/zy/matchgame/entity/Response.java
@@ -0,0 +1,17 @@
+package com.zy.matchgame.entity;
+
+/**
+ - @title 响应实体父类
+ */
+public class Response {
+	
+	private String code;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+}
