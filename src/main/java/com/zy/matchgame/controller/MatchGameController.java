38980bef7d623b9a5cb638580c1f345241261ca5
TingYan <xym13662613887@outlook.com>
2024-08-05 16:33:24 +0800

    获取用户名

--- src/main/java/com/zy/matchgame/controller/MatchGameController.java
+++ src/main/java/com/zy/matchgame/controller/MatchGameController.java
@@ -1,10 +1,18 @@
 package com.zy.matchgame.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/matchgame")
+@RequestMapping("/matching")
 public class MatchGameController {
+
+    @GetMapping("/getUsername")
+    public String getUsername(HttpSession httpSession) {
+        return (String) httpSession.getAttribute("username");
+    }
+
 
 }
