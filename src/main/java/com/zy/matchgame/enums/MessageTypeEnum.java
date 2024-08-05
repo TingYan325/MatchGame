f41cdce361fef28bf88068e5e83744610c06006a
TingYan <xym13662613887@outlook.com>
2024-08-05 13:40:24 +0800

    消息类型枚举类

---
+++ src/main/java/com/zy/matchgame/enums/MessageTypeEnum.java
@@ -0,0 +1,29 @@
+package com.zy.matchgame.enums;
+
+/**
+ * 消息类型
+ */
+
+public enum MessageTypeEnum {
+
+    /**
+     * 用户加入
+     */
+    ADD_USER,
+    /**
+     * 匹配对手
+     */
+    MATCH_USER,
+    /**
+     * 取消匹配
+     */
+    CANCEL_MATCH,
+    /**
+     * 游戏开始
+     */
+    PLAY_GAME,
+    /**
+     * 游戏结束
+     */
+    GAME_OVER,
+}
