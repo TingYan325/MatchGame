7eaf56cc5249e6d60be185a264836ebf0d4345ae
TingYan <xym13662613887@outlook.com>
2024-08-05 16:05:01 +0800

    业务逻辑实现类

---
+++ src/main/java/com/zy/matchgame/domain/LogicImpl.java
@@ -0,0 +1,20 @@
+package com.zy.matchgame.domain;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogicImpl {
+    public void addUser(JSONObject jsonObject) {
+        return;
+    }
+
+    public void playGame(JSONObject jsonObject) {
+    }
+
+    public void matchUser(JSONObject jsonObject) {
+    }
+
+    public void gameOver(JSONObject jsonObject) {
+    }
+}
