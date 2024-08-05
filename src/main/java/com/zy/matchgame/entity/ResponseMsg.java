d816a09e248c2bbf0eea9327ddd3fe883f4dd580
TingYan <xym13662613887@outlook.com>
2024-08-05 13:34:42 +0800

    消息响应实体类

---
+++ src/main/java/com/zy/matchgame/entity/ResponseMsg.java
@@ -0,0 +1,17 @@
+package com.zy.matchgame.entity;
+
+/**
+ * @title 消息响应实体类
+ */
+public class ResponseMsg extends Response {
+
+	private String msg;
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+}
