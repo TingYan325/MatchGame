a322fb779c68150cb0f82d182edc3611d177258b
TingYan <xym13662613887@outlook.com>
2024-08-06 09:14:29 +0800

    添加登陆方法

--- src/main/java/com/zy/matchgame/controller/MatchGameController.java
+++ src/main/java/com/zy/matchgame/controller/MatchGameController.java
@@ -1,18 +1,31 @@
 package com.zy.matchgame.controller;
 
+import com.zy.matchgame.entity.Response;
+import com.zy.matchgame.utils.ResponseUtil;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/matching")
 public class MatchGameController {
+
+    @Autowired
+    ResponseUtil responseUtil;
 
     @GetMapping("/getUsername")
     public String getUsername(HttpSession httpSession) {
         return (String) httpSession.getAttribute("username");
     }
 
-
+    @PostMapping("/login")
+    public Response<?> login(@RequestBody User user, HttpSession httpSession) {
+        if(user.getUsername() != null && user.getPassword() == "123") {
+            httpSession.setAttribute("username", user.getUsername());
+            return responseUtil.response_Success(user.getUsername());
+        } else {
+            return responseUtil.response_Faild(user.getUsername());
+        }
+    }
 }
