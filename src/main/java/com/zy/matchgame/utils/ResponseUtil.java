05c30682b233e964e452842d173e3a64121df9ed
TingYan <xym13662613887@outlook.com>
2024-08-06 10:15:41 +0800

    添加登录了失败的消息响应实体类和生成添加用户的响应消息实体类方法

--- src/main/java/com/zy/matchgame/utils/ResponseUtil.java
+++ src/main/java/com/zy/matchgame/utils/ResponseUtil.java
@@ -4,6 +4,8 @@ import com.zy.matchgame.entity.Response;
 import com.zy.matchgame.entity.ResponseMsg;
 import com.zy.matchgame.enums.MessageCode;
 import com.zy.matchgame.enums.MessageTypeEnum;
+import com.zy.matchgame.enums.StatusEnum;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
@@ -11,6 +13,10 @@ import java.util.Set;
 
 @Component
 public class ResponseUtil {
+
+    @Autowired
+    MatchUtil matchUtil;
+
     /**
      * 系统信息回复成功
      * @param userName
@@ -20,7 +26,7 @@ public class ResponseUtil {
         Response response = new Response();
         ResponseMsg<String> responseMsg = new ResponseMsg<>();
         //设置respond里面的响应码和描述
-        response.setCode(MessageCode.SUCCESS.getCode().toString());
+        response.setCode(MessageCode.SUCCESS.getCode());
         response.setDesc(MessageCode.SUCCESS.getDesc());
         //设置respondMsg里面的消息类型为系统信息
         responseMsg.setType(MessageTypeEnum.SYSTEM);
@@ -44,7 +50,7 @@ public class ResponseUtil {
         Response response = new Response();
         ResponseMsg<String> responseMsg = new ResponseMsg<>();
         //设置respond里面的响应码和描述
-        response.setCode(MessageCode.SUCCESS.getCode().toString());
+        response.setCode(MessageCode.SUCCESS.getCode());
         response.setDesc(MessageCode.SUCCESS.getDesc());
         //设置respondMsg里面的消息类型为系统信息
         responseMsg.setType(MessageTypeEnum.SYSTEM);
@@ -56,4 +62,63 @@ public class ResponseUtil {
         return response;
     }
 
+    /**
+     * 生成登陆失败的消息响应实体类
+     * @param userName
+     * @return
+     */
+    public Response<String> response_Faild(String userName) {
+        Response response = new Response();
+        ResponseMsg<String> responseMsg = new ResponseMsg<>();
+        //设置respond里面的响应码和描述
+        response.setCode(MessageCode.PASSWORD_ERROR.getCode());
+        response.setDesc(MessageCode.PASSWORD_ERROR.getDesc());
+        //设置respondMsg里面的消息类型为系统信息
+        responseMsg.setType(MessageTypeEnum.SYSTEM);
+        //设置respondMsg里面的发送者为NULL
+        responseMsg.setSender("NULL");
+        //设置respondMsg里面的接收者为该用户
+        Set<String> set = new HashSet<>();
+        set.add(userName);
+        responseMsg.setReceivers(set);
+        response.setResponseMsg(responseMsg);
+        return response;
+    }
+
+
+    /**
+     * 生成添加添加用户用户的响应消息实体类
+     * @param userName
+     * @return
+     */
+    public Response<Object> response_AddUser(String userName) {
+        StatusEnum status = matchUtil.getOnlineStatus(userName);
+
+        Response<Object> response = new Response<>();
+        ResponseMsg<Object> responseMsg = new ResponseMsg<>();
+        //设置接收人，消息类型和发送人为系统
+        responseMsg.setType(MessageTypeEnum.ADD_USER);
+        responseMsg.setSender("NULL");
+        Set<String> set = new HashSet<>();
+        set.add(userName);
+        responseMsg.setReceivers(set);
+
+        /**
+         * 根据用户状态不同返回不同类型的消息码和描述
+         */
+        if(status != null) {
+            if(status.compareTo(StatusEnum.GAME_OVER) == 0){
+                response.setCode(MessageCode.SUCCESS.getCode());
+                response.setDesc(MessageCode.SUCCESS.getDesc());
+            } else {
+                response.setCode(MessageCode.USER_IS_ONLINE.getCode());
+                response.setDesc(MessageCode.USER_IS_ONLINE.getDesc());
+            }
+            response.setCode(MessageCode.SUCCESS.getCode());
+            response.setDesc(MessageCode.SUCCESS.getDesc());
+        }
+        response.setResponseMsg(responseMsg);
+
+        return response;
+    }
 }
