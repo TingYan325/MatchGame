8208ef3c3b3202ff22081663b7ef46c488e98b1d
TingYan <xym13662613887@outlook.com>
2024-08-06 12:58:52 +0800

    服务层实现

--- src/main/java/com/zy/matchgame/service/impl/QuestionServiceImpl.java
+++ src/main/java/com/zy/matchgame/service/impl/QuestionServiceImpl.java
@@ -1,10 +1,25 @@
 package com.zy.matchgame.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zy.matchgame.dao.QuestionDao;
 import com.zy.matchgame.entity.Question;
 import com.zy.matchgame.mapper.QuestionMapper;
 import com.zy.matchgame.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class QuestionServiceImpl extends ServiceImpl<QuestionMapper, Question> implements QuestionService {
+    @Autowired
+    private QuestionDao questionDao;
 
+    /**
+     * 获取问题
+     */
+    @Override
+    public List<Question> getAllQuestion() {
+        return questionDao.getAllQuestion();
+    }
 }
