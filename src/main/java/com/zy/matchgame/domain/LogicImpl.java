0bc376b8fa9734cba319bc8cd5e23fb05a9c9592
TingYan <xym13662613887@outlook.com>
2024-08-06 10:11:43 +0800

    添加了添加用户的实现逻辑方法

--- src/main/java/com/zy/matchgame/domain/LogicImpl.java
+++ src/main/java/com/zy/matchgame/domain/LogicImpl.java
@@ -1,18 +1,55 @@
 package com.zy.matchgame.domain;
 
 import com.alibaba.fastjson.JSONObject;
+import com.zy.matchgame.entity.Response;
+import com.zy.matchgame.enums.StatusEnum;
+import com.zy.matchgame.utils.MatchUtil;
+import com.zy.matchgame.utils.ResponseUtil;
+import com.zy.matchgame.websocket.MatchEndPoint;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class LogicImpl {
+    @Autowired
+    MatchEndPoint matchEndPoint;
+
+    @Autowired
+    ResponseUtil responseUtil;
+
+    private final MatchUtil matchUtil;
+
+    public LogicImpl(MatchUtil matchUtil) {
+        this.matchUtil = matchUtil;
+    }
+
     public void addUser(JSONObject jsonObject) {
-        return;
+        log.info("ChatWebsocket addUser 用户加入游戏开始 message: {}", jsonObject.toJSONString());
+
+        String username = jsonObject.getString("username");
+        StatusEnum statusEnum = matchUtil.getOnlineStatus(username);
+        Response<?> response = responseUtil.response_AddUser(username);
+
+        if(statusEnum != null) {
+            if(statusEnum.compareTo(StatusEnum.GAME_OVER) == 0) {
+                matchUtil.setOnlineStatus_IDLE(username);
+            }
+        } else {
+            matchUtil.setOnlineStatus_IDLE(username);
+        }
+
+        matchEndPoint.sendToUser(response);
+
+        log.info("ChatWebsocket addUser 用户加入游戏结束 message: {}", jsonObject.toJSONString());
     }
 
     public void playGame(JSONObject jsonObject) {
     }
 
     public void matchUser(JSONObject jsonObject) {
+
     }
 
     public void gameOver(JSONObject jsonObject) {
