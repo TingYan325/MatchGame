591a0ea0881dd8ff18ede9e2d53139054659bbe6
TingYan <xym13662613887@outlook.com>
2024-08-06 10:49:06 +0800

    常量类

---
+++ src/main/java/com/zy/matchgame/constant/CommonField.java
@@ -0,0 +1,11 @@
+package com.zy.matchgame.constant;
+
+/**
+ * 常量类
+ */
+public class CommonField {
+
+    public static final String MATCH_TASK_NAME_PREFIX = "MatchTask -- ";
+
+    public static final int QUESTION_RANDOM_COUNT = 10;
+}
