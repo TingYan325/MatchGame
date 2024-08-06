a7f3615f8169340c74171a652f763e4ea5f75a4a
TingYan <xym13662613887@outlook.com>
2024-08-06 12:59:30 +0800

    用户实体类

---
+++ src/main/java/com/zy/matchgame/enums/User.java
@@ -0,0 +1,14 @@
+package com.zy.matchgame.enums;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    String Username;
+    String Password;
+}
