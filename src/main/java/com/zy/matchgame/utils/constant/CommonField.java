0c8475410b24fdf5b6d441168e964240011afb1b
TingYan <xym13662613887@outlook.com>
2024-08-08 16:54:22 +0800

    修改了在数据库随机取题目的逻辑

--- src/main/java/com/zy/matchgame/utils/constant/CommonField.java
+++ src/main/java/com/zy/matchgame/utils/constant/CommonField.java
@@ -7,5 +7,5 @@ public class CommonField {
 
     public static final String MATCH_TASK_NAME_PREFIX = "MatchTask -- ";
 
-    public static final int QUESTION_RANDOM_COUNT = 10;
+    public static final int QUESTION_RANDOM_COUNT = 1;
 }
